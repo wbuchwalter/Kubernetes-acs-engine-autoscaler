f8cb24eed3ae4560a9e628ff6b8f09e5ed2bf050
Vicki Cheung <vicki@openai.com>
2016-08-30 08:55:32 -0700

    Initial release

---
+++ autoscaler/kube.py
@@ -0,0 +1,213 @@
+import json
+import logging
+
+import pykube.exceptions
+
+import autoscaler.utils as utils
+
+logger = logging.getLogger(__name__)
+
+
+class KubePodStatus(object):
+    RUNNING = 'Running'
+    PENDING = 'Pending'
+    CONTAINER_CREATING = 'ContainerCreating'
+    SUCCEEDED = 'Succeeded'
+    FAILED = 'Failed'
+
+
+class KubePod(object):
+    def __init__(self, pod):
+        self.original = pod
+
+        metadata = pod.obj['metadata']
+        self.name = metadata['name']
+        self.namespace = metadata['namespace']
+        self.node_name = pod.obj['spec'].get('nodeName', None)
+        self.status = pod.obj['status']['phase']
+        self.uid = metadata['uid']
+        self.selectors = pod.obj['spec'].get('nodeSelector', {})
+        self.labels = metadata.get('labels', {})
+        self.annotations = metadata.get('annotations', {})
+
+        # TODO: refactor
+        requests = map(lambda c: c.get('resources', {}).get('requests', {}),
+                       pod.obj['spec']['containers'])
+        resource_requests = {}
+        for d in requests:
+            for k, v in d.iteritems():
+                unitless_v = utils.parse_SI(v)
+                resource_requests[k] = resource_requests.get(k, 0.0) + unitless_v
+        self.resources = KubeResource(pods=1, **resource_requests)
+
+    def is_mirrored(self):
+        created_by = json.loads(self.annotations.get('kubernetes.io/created-by', '{}'))
+        is_daemonset = created_by.get('reference', {}).get('kind') == 'DaemonSet'
+        return is_daemonset or self.annotations.get('kubernetes.io/config.mirror')
+
+    def __hash__(self):
+        return hash(self.uid)
+
+    def __eq__(self, other):
+        return self.uid == other.uid
+
+    def __str__(self):
+        return 'KubePod({namespace}, {name})'.format(
+            namespace=self.namespace, name=self.name)
+
+    def __repr__(self):
+        return str(self)
+
+
+class KubeNode(object):
+    def __init__(self, node):
+        self.original = node
+        self.pykube_node = node
+
+        metadata = node.obj['metadata']
+        self.name = metadata['name']
+        self.instance_id, self.region, self.instance_type = self._get_instance_data()
+        self.selectors = metadata['labels']
+
+        self.capacity = KubeResource(**node.obj['status']['capacity'])
+        self.used_capacity = KubeResource()
+        self.unschedulable = node.obj['spec'].get('unschedulable', False)
+
+    def _get_instance_data(self):
+        """
+        returns a tuple (instance id, region, instance type)
+        """
+        labels = self.original.obj['metadata']['labels']
+        instance_type = labels.get('aws/type', labels.get('beta.kubernetes.io/instance-type'))
+
+        provider = self.original.obj['spec'].get('providerID', '')
+        if provider.startswith('aws://'):
+            az, instance_id = tuple(provider.split('/')[-2:])
+            return (instance_id, az[:-1], instance_type)
+
+        if labels.get('aws/id') and labels.get('aws/az'):
+            instance_id = labels['aws/id']
+            region = labels['aws/az'][:-1]
+            return (instance_id, region, instance_type)
+
+        return (None, '', None)
+
+    def uncordon(self):
+        try:
+            self.original.reload()
+            self.original.obj['spec']['unschedulable'] = False
+            self.original.update()
+            logger.info("uncordon {}".format(self))
+            return True
+        except pykube.exceptions.HTTPError as ex:
+            logger.info("uncordon failed {} {}".format(self, ex))
+            return False
+
+    def cordon(self):
+        try:
+            self.original.reload()
+            self.original.obj['spec']['unschedulable'] = True
+            self.original.update()
+            logger.info("cordon {}".format(self))
+            return True
+        except pykube.exceptions.HTTPError as ex:
+            logger.info("cordon failed {} {}".format(self, ex))
+            return False
+
+    def delete(self):
+        try:
+            self.original.delete()
+            logger.info("delete {}".format(self))
+            return True
+        except pykube.exceptions.HTTPError as ex:
+            logger.info("delete failed {} {}".format(self, ex))
+            return False
+
+    def count_pod(self, pod):
+        assert isinstance(pod, KubePod)
+        self.used_capacity += pod.resources
+
+    def can_fit(self, resources):
+        assert isinstance(resources, KubeResource)
+        left = self.used_capacity + resources - self.capacity
+        return left.possible
+
+    def is_match(self, pod):
+        """
+        whether this node matches all the selectors on the pod
+        """
+        for label, value in pod.selectors.iteritems():
+            if self.selectors.get(label) != value:
+                return False
+        return True
+
+    def is_managed(self):
+        """
+        an instance is managed if we know its instance ID in ec2.
+        """
+        return self.instance_id is not None
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return "{}: {} ({})".format(self.name, self.instance_id,
+                                    utils.selectors_to_hash(self.selectors))
+
+
+class KubeResource(object):
+
+    def __init__(self, **kwargs):
+        self.raw = dict((k, utils.parse_resource(v))
+                        for (k, v) in kwargs.iteritems())
+
+    def __add__(self, other):
+        keys = set(self.raw.iterkeys()) | set(other.raw.iterkeys())
+        raw_diff = dict((k, self.raw.get(k, 0) + other.raw.get(k, 0))
+                        for k in keys)
+        return KubeResource(**raw_diff)
+
+    def __sub__(self, other):
+        keys = set(self.raw.iterkeys()) | set(other.raw.iterkeys())
+        raw_diff = dict((k, self.raw.get(k, 0) - other.raw.get(k, 0))
+                        for k in keys)
+        return KubeResource(**raw_diff)
+
+    def __mul__(self, multiplier):
+        new_raw = dict((k, v * multiplier) for k, v in self.raw.iteritems())
+        return KubeResource(**new_raw)
+
+    def __rmul__(self, multiplier):
+        return self.__mul__(multiplier)
+
+    def __cmp__(self, other):
+        """
+        should return a negative integer if self < other,
+        zero if self == other, a positive integer if self > other.
+
+        we consider self to be greater than other if it exceeds
+        the resource amount in other in more resource types.
+        e.g. if self = {cpu: 4, memory: 1K, gpu: 1},
+        other = {cpu: 2, memory: 2K}, then self exceeds the resource
+        amount in other in both cpu and gpu, while other exceeds
+        the resource amount in self in only memory, so self > other.
+        """
+        resource_diff = (self - other).raw
+        num_resource_types = len(resource_diff)
+        num_eq = sum(1 for v in resource_diff.itervalues() if v == 0)
+        num_less = sum(1 for v in resource_diff.itervalues() if v < 0)
+        num_more = num_resource_types - num_eq - num_less
+        return num_more - num_less
+
+    def __str__(self):
+        return str(self.raw)
+
+    def get(self, key, default=None):
+        return self.raw.get(key, default)
+
+    @property
+    def possible(self):
+        return all(map(lambda x: x >= 0, self.raw.itervalues()))
---
+++ test/test_cluster.py
@@ -0,0 +1,211 @@
+import os.path
+import unittest
+
+import boto3
+import pykube
+import mock
+import moto
+import yaml
+
+from autoscaler.cluster import Cluster
+from autoscaler.kube import KubePod, KubeNode
+import autoscaler.utils as utils
+
+
+class TestCluster(unittest.TestCase):
+    def setUp(self):
+        # load dummy kube specs
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(dir_path, 'data/busybox.yaml'), 'r') as f:
+            self.dummy_pod = yaml.load(f.read())
+        with open(os.path.join(dir_path, 'data/ds-pod.yaml'), 'r') as f:
+            self.dummy_ds_pod = yaml.load(f.read())
+        with open(os.path.join(dir_path, 'data/rc-pod.yaml'), 'r') as f:
+            self.dummy_rc_pod = yaml.load(f.read())
+        with open(os.path.join(dir_path, 'data/node.yaml'), 'r') as f:
+            self.dummy_node = yaml.load(f.read())
+
+        # this isn't actually used here
+        # only needed to create the KubePod object...
+        self.api = pykube.HTTPClient(pykube.KubeConfig.from_file('~/.kube/config'))
+
+        # start creating our mock ec2 environment
+        self.mocks = [moto.mock_ec2(), moto.mock_autoscaling()]
+        for moto_mock in self.mocks:
+            moto_mock.start()
+
+        client = boto3.client('autoscaling')
+        self.asg_client = client
+
+        client.create_launch_configuration(
+            LaunchConfigurationName='dummy-lc',
+            ImageId='ami-deadbeef',
+            KeyName='dummy-key',
+            SecurityGroups=[
+                'sg-cafebeef',
+            ],
+            InstanceType='t2.medium'
+        )
+
+        client.create_auto_scaling_group(
+            AutoScalingGroupName='dummy-asg',
+            LaunchConfigurationName='dummy-lc',
+            MinSize=0,
+            MaxSize=10,
+            VPCZoneIdentifier='subnet-beefbeef',
+            Tags=[
+                {
+                    'Key': 'KubernetesCluster',
+                    'Value': 'dummy-cluster',
+                    'PropagateAtLaunch': True
+                },
+                {
+                    'Key': 'KubernetesRole',
+                    'Value': 'worker',
+                    'PropagateAtLaunch': True
+                }
+            ]
+        )
+
+        # finally our cluster
+        self.cluster = Cluster(
+            aws_access_key='',
+            aws_secret_key='',
+            regions=['us-west-2', 'us-east-1', 'us-west-1'],
+            kubeconfig='~/.kube/config',
+            idle_threshold=60,
+            instance_init_time=60,
+            type_idle_threshold=60,
+            cluster_name='dummy-cluster',
+            slack_hook='',
+            dry_run=False
+        )
+
+    def tearDown(self):
+        for moto_mock in self.mocks:
+            moto_mock.stop()
+
+    def _spin_up_node(self):
+        # spin up dummy ec2 node
+        self.asg_client.set_desired_capacity(AutoScalingGroupName='dummy-asg',
+                                             DesiredCapacity=1)
+        response = self.asg_client.describe_auto_scaling_groups()
+        instance_id = response['AutoScalingGroups'][0]['Instances'][0]['InstanceId']
+
+        self.dummy_node['metadata']['labels']['aws/id'] = instance_id
+        node = KubeNode(pykube.Node(self.api, self.dummy_node))
+        return node
+
+    def test_scale_up_selector(self):
+        self.dummy_pod['spec']['nodeSelector'] = {
+            'aws/type': 'm4.large'
+        }
+        pod = KubePod(pykube.Pod(self.api, self.dummy_pod))
+        selectors_hash = utils.selectors_to_hash(pod.selectors)
+        asgs = self.cluster.autoscaling_groups.get_all_groups([])
+        self.cluster.fulfill_pending(asgs, selectors_hash, pod.resources, [pod])
+
+        response = self.asg_client.describe_auto_scaling_groups()
+        assert len(response['AutoScalingGroups']) == 1
+        assert response['AutoScalingGroups'][0]['DesiredCapacity'] == 0
+
+    def test_scale_up(self):
+        pod = KubePod(pykube.Pod(self.api, self.dummy_pod))
+        selectors_hash = utils.selectors_to_hash(pod.selectors)
+        asgs = self.cluster.autoscaling_groups.get_all_groups([])
+        self.cluster.fulfill_pending(asgs, selectors_hash, pod.resources, [pod])
+
+        response = self.asg_client.describe_auto_scaling_groups()
+        assert len(response['AutoScalingGroups']) == 1
+        assert response['AutoScalingGroups'][0]['DesiredCapacity'] > 0
+
+    def test_scale_down(self):
+        """
+        kube node with daemonset and no pod --> cordon
+        """
+        node = self._spin_up_node()
+        node.cordon = mock.Mock(return_value="mocked stuff")
+        all_nodes = [node]
+        managed_nodes = [n for n in all_nodes if node.is_managed()]
+        running_insts_map = self.cluster.get_running_instances_map(managed_nodes)
+        pods_to_schedule = {}
+        asgs = self.cluster.autoscaling_groups.get_all_groups(all_nodes)
+
+        ds_pod = KubePod(pykube.Pod(self.api, self.dummy_ds_pod))
+        running_or_pending_assigned_pods = [ds_pod]
+
+        self.cluster.idle_threshold = -1
+        self.cluster.type_idle_threshold = -1
+        self.cluster.maintain(
+            managed_nodes, running_insts_map,
+            pods_to_schedule, running_or_pending_assigned_pods, asgs)
+
+        response = self.asg_client.describe_auto_scaling_groups()
+        assert len(response['AutoScalingGroups']) == 1
+        assert response['AutoScalingGroups'][0]['DesiredCapacity'] == 1
+        node.cordon.assert_called_once_with()
+
+    def test_scale_down_grace_period(self):
+        """
+        kube node with daemonset and no pod + grace period --> noop
+        """
+        node = self._spin_up_node()
+        node.cordon = mock.Mock(return_value="mocked stuff")
+        all_nodes = [node]
+        managed_nodes = [n for n in all_nodes if node.is_managed()]
+        running_insts_map = self.cluster.get_running_instances_map(managed_nodes)
+        pods_to_schedule = {}
+        asgs = self.cluster.autoscaling_groups.get_all_groups(all_nodes)
+
+        # kube node with daemonset and no pod --> cordon
+        ds_pod = KubePod(pykube.Pod(self.api, self.dummy_ds_pod))
+        running_or_pending_assigned_pods = [ds_pod]
+
+        self.cluster.maintain(
+            managed_nodes, running_insts_map,
+            pods_to_schedule, running_or_pending_assigned_pods, asgs)
+
+        response = self.asg_client.describe_auto_scaling_groups()
+        assert len(response['AutoScalingGroups']) == 1
+        assert response['AutoScalingGroups'][0]['DesiredCapacity'] == 1
+        node.cordon.assert_not_called()
+
+    def test_scale_down_busy(self):
+        """
+        kube node with daemonset and pod/rc-pod --> noop
+        """
+        node = self._spin_up_node()
+        node.cordon = mock.Mock(return_value="mocked stuff")
+        all_nodes = [node]
+        managed_nodes = [n for n in all_nodes if node.is_managed()]
+        running_insts_map = self.cluster.get_running_instances_map(managed_nodes)
+        pods_to_schedule = {}
+        asgs = self.cluster.autoscaling_groups.get_all_groups(all_nodes)
+
+        # kube node with daemonset and no pod --> cordon
+        ds_pod = KubePod(pykube.Pod(self.api, self.dummy_ds_pod))
+        pod = KubePod(pykube.Pod(self.api, self.dummy_pod))
+        running_or_pending_assigned_pods = [ds_pod, pod]
+
+        self.cluster.maintain(
+            managed_nodes, running_insts_map,
+            pods_to_schedule, running_or_pending_assigned_pods, asgs)
+
+        response = self.asg_client.describe_auto_scaling_groups()
+        assert len(response['AutoScalingGroups']) == 1
+        assert response['AutoScalingGroups'][0]['DesiredCapacity'] == 1
+        node.cordon.assert_not_called()
+
+        # kube node with daemonset and rc pod --> noop
+        ds_pod = KubePod(pykube.Pod(self.api, self.dummy_ds_pod))
+        rc_pod = KubePod(pykube.Pod(self.api, self.dummy_rc_pod))
+        running_or_pending_assigned_pods = [ds_pod, rc_pod]
+
+        self.cluster.maintain(
+            managed_nodes, running_insts_map,
+            pods_to_schedule, running_or_pending_assigned_pods, asgs)
+
+        response = self.asg_client.describe_auto_scaling_groups()
+        assert len(response['AutoScalingGroups']) == 1
+        assert response['AutoScalingGroups'][0]['DesiredCapacity'] == 1
+        node.cordon.assert_not_called()
