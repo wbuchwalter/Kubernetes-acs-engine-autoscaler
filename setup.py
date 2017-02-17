2635d3e3efac3499b8e56a3cb3afd6f9e4d51b0f
Aleks Kamko <aykamko@gmail.com>
2017-02-16 17:10:43 -0800

    add find_packages() to setup.py (#21)

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='autoscaler',
       version='0.0.1',
+      packages=find_packages(),
       install_requires=[
           'pykube',
           'requests',
