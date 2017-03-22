3fb4dee397d15b92951d27ed87d660f00127dab8
William Buchwalter <wbuchwalter@gmail.com>
2017-03-22 11:07:05 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 az account clear
-python main.py --container_service_name 'containerservice-kub' --resource_group 'kub' --service_principal_app_id '349a988c-8957-4f86-bb94-d4a9b366eea1' --service_principal_secret 'xxxx' --service_principal_tenant_id 'xxxx' -vvv --kubeconfig /root/.kube/config --sleep 5 --over-provision 0 --debug
+python main.py --container_service_name 'containerservice-kub' --resource_group 'kub' --service_principal_app_id 'xxx' --service_principal_secret 'xxxx' --service_principal_tenant_id 'xxxx' -vvv --kubeconfig /root/.kube/config --sleep 5 --over-provision 0 --debug
