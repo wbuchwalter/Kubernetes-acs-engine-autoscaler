dc9b0532f8d91a4a13d85847b332db7495a34b44
William Buchwalter <wbuchwalter@gmail.com>
2017-05-17 13:11:39 -0700

    fix unit tests

--- test/test_engine_scaler.py
+++ test/test_engine_scaler.py
@@ -8,10 +8,10 @@ import copy
 from datetime import datetime, timedelta
 import pykube
 from autoscaler.kube import KubePod, KubeNode, KubeResource
-from autoscaler.container_service import ContainerService
+from autoscaler.engine_scaler import EngineScaler
 from unittest.mock import MagicMock
 
-class TestContainerService(unittest.TestCase):
+class TestEngineScaler(unittest.TestCase):
     def setUp(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         with open(os.path.join(dir_path, 'data/node.yaml'), 'r') as f:
@@ -20,16 +20,16 @@ class TestContainerService(unittest.TestCase):
             self.dummy_pod = yaml.load(f.read())
         self.api = pykube.HTTPClient(pykube.KubeConfig.from_file('~/.kube/config'))
     
-    def create_container_service(self, nodes):
-        return ContainerService( 
+    def create_engine_scaler(self, nodes):
+        return EngineScaler( 
             resource_group='my-rg',
             nodes=nodes,            
             deployments=None,
             dry_run=False,
-            container_service_name=None,
             arm_template='fake',
             arm_parameters='fake',
-            over_provision=0)
+            over_provision=0,
+            spare_count=1)
 
     def create_nodes(self, pool_size, nb_nodes):
         nodes = []
@@ -45,15 +45,15 @@ class TestContainerService(unittest.TestCase):
     
     def test_get_agent_pools(self):
         nodes = self.create_nodes(2,1)
-        container_service = self.create_container_service(nodes)
+        engine_scaler = self.create_engine_scaler(nodes)
         
-        pools = container_service.get_agent_pools(nodes)
+        pools = engine_scaler.get_agent_pools(nodes)
         self.assertEqual(len(pools), 2)    
         self.assertEqual(pools[0].actual_capacity, 1)
         self.assertEqual(pools[1].actual_capacity, 1)
 
         nodes = self.create_nodes(3,3)
-        pools = container_service.get_agent_pools(nodes)
+        pools = engine_scaler.get_agent_pools(nodes)
         self.assertEqual(len(pools), 3)    
         self.assertEqual(pools[0].actual_capacity, 3)
         self.assertEqual(pools[1].actual_capacity, 3)
@@ -61,12 +61,12 @@ class TestContainerService(unittest.TestCase):
     
     def test_fulfill_pending(self):
         nodes = self.create_nodes(2,1)
-        container_service = self.create_container_service(nodes)
+        engine_scaler = self.create_engine_scaler(nodes)
         pod = KubePod(pykube.Pod(self.api, self.dummy_pod))
-        container_service.scale_pools = MagicMock()
+        engine_scaler.scale_pools = MagicMock()
 
-        container_service.fulfill_pending([pod])
-        container_service.scale_pools.assert_called_with({'agentpool0': 2, 'agentpool1': 1}, True)
+        engine_scaler.fulfill_pending([pod])
+        engine_scaler.scale_pools.assert_called_with({'agentpool0': 2, 'agentpool1': 1}, True)
         
 
         
